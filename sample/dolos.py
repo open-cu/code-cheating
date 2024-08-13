2bbb2a34d5030d0995ae8c9ac0c327941b1fc176
Vladimir <139348193+redisner@users.noreply.github.com>
2024-08-13 12:44:08 +0300

    add dolos submodule and instructions to run Dolos API

--- sample/dolos.py
+++
@@ -1,8 +0,0 @@
-import os
-
-base_command = 'docker run --init -v "$PWD:/dolos" ghcr.io/dodona-edu/dolos-cli -l python -f csv '
-
-tasks_folder = input("tasks folder: ")
-task_name = input("task name: ")
-
-os.system(base_command + f"{tasks_folder}/{task_name}/*.py")
